389ece15a1763d785831864a83897b3fb3b8d4d7
Artem Polyakov <artpol84@gmail.com>
2020-10-07 08:49:31 -0700

    Few debugging items

--- drivers/vfio/vfio_iommu_type1.c
+++ drivers/vfio/vfio_iommu_type1.c
@@ -2605,7 +2605,8 @@ static int vfio_iommu_migration_build_caps(struct vfio_iommu *iommu,
 	/* support minimum pgsize */
 	cap_mig.pgsize_bitmap = (size_t)1 << __ffs(iommu->pgsize_bitmap);
 	cap_mig.max_dirty_bitmap_size = DIRTY_BITMAP_SIZE_MAX;
-    pr_err("cap_mig.pgsize_bitmap = %x", cap_mig.pgsize_bitmap);
+    pr_err("cap_mig.pgsize_bitmap = %llx, ps_bitmap = %llx", 
+            (uint64_t)cap_mig.pgsize_bitmap, (uint64_t)iommu->pgsize_bitmap);
 
 	return vfio_info_add_capability(caps, &cap_mig.header, sizeof(cap_mig));
 }
@@ -2641,7 +2642,7 @@ static int vfio_iommu_type1_get_info(struct vfio_iommu *iommu,
 	info.iova_pgsizes = iommu->pgsize_bitmap;
 
 	ret = vfio_iommu_migration_build_caps(iommu, &caps);
-
+    pr_err("vfio_iommu_migration_build_caps, ret = %d", ret);
 	if (!ret)
 		ret = vfio_iommu_iova_build_caps(iommu, &caps);
 
